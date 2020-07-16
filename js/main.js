b503480f4f2256e919ccc9db4496779424420f87
Gwen <gwen.nadia@gmail.com>
2020-07-16 02:38:25 +0200

    Fichiers JS
    attribut alt

--- js/main.js
+++ js/main.js
@@ -3,12 +3,18 @@
    let cardHeaderTitle = document.createElement('h1');
    document.body.prepend(cardHeader);
    cardHeader.prepend(cardHeaderTitle);
-   cardHeaderTitle.textContent = "Orinoco :" + "     " + "Des meubles d'exception";
+   cardHeaderTitle.textContent = "     " + "Des meubles d'exception";
    let b = document.body;
    let cardSection = document.createElement('main');
    b.appendChild(cardSection);
    cardSection.classList.add('slideshow-container');
- 
+   let PageFooter = document.createElement('footer');
+   document.body.appendChild(PageFooter);
+   let PageFooterDiv = document.createElement('div');
+   PageFooter.prepend(PageFooterDiv);
+   let PageFooterText = document.createElement('p');
+   PageFooterDiv.prepend(PageFooterText);
+   PageFooterText.textContent = '💻 Site créé en juillet 2020 par Orinoco - 29 avenue des Peupliers 35510 Cesson-Sévigné'
  // utilisation d'une fonction then pour exécuter du code, c'est une promesse
  
  fetch ('http://localhost:3000/api/furniture').then(response =>
@@ -16,35 +22,36 @@
         
          // pour reprendre plus tard dans for, je détermine les données, leurs noms
  
-         let cardDiv = document.createElement('div');
+         let cardDiv = document.createElement('div'); //
          let cardTitle = [];
          let cardImg = [];
          let cardId = [];
-         let cardArticle = [];
- 
+         let cardDescription = [];
+        // let productView = []; // document.createElement('a')
+
          cardSection.prepend(cardDiv);
-         cardDiv.classList.add('mySlides', 'fade');
-         let cardFooter = document.createElement('footer');
-         document.body.appendChild(cardFooter);
  
          // boucle pour exécuter et attribuer
          for (var i = 0; i < response.length; i++) {
-           // la div n'englobe pas les éléments suivants
+            // la div n'englobe pas les éléments suivants, et le a le se multiplie pas
+         //  cardDiv.concat(document.createElement('div'));
            cardTitle.push(document.createElement('h2'));
            cardImg.push(document.createElement('img'));
            cardId.push(document.createElement('figcaption'));
-           cardArticle.push(document.createElement('p'));;
-           // cardSection[i].appendChild(cardDiv[i]);
- 
-           cardTitle[i].textContent = response[i].name + ' - ' + response[i].price + ' €';
+           cardDescription.push(document.createElement('p'));;
+           // cardDiv.classList.add('mySlides', 'fade');
+          // cardDiv[i].childNodes = response[i] ;
+           cardTitle[i].textContent = response[i].name + ' - ' + response[i].price/100 + ' € TTC';
            cardDiv.appendChild(cardTitle[i]);
            cardImg[i].src = response[i].imageUrl;
            cardDiv.appendChild(cardImg[i]);
+           cardImg[i].setAttribute('alt', 'meuble en bois');
            cardId[i].textContent = 'ref article : ' + response[i]._id;
            cardDiv.appendChild(cardId[i]);
-           cardArticle[i].textContent = response[i].description;
-           cardDiv.appendChild(cardArticle[i]);
- 
+           cardDescription[i].textContent = response[i].description;
+           cardDiv.appendChild(cardDescription[i]);
+         // productView.textContent = 'En savoir plus';
+         //  cardDiv.appendChild(productView);
  // try catch
  
  
